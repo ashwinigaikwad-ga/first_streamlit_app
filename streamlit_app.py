a3559aa913e6c3fcc1328221fb5ed4b8a3970648
ashwinigaikwad-ga <155563722+ashwinigaikwad-ga@users.noreply.github.com>
2024-01-04 17:37:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,15 +11,16 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 streamlit.dataframe(my_fruit_list)
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+# fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),default=['Avocado', 'Strawberries'])
 # fruits_selected= streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), default=['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
 my_fruit_lists = my_fruit_list.set_index('fruits_to_show')
 
+assert set(defaultcols).issubset(df.columns)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
