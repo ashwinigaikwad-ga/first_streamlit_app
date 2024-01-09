a90782b20ae6575b95736757a0f15492af8d6f66
ashwinigaikwad-ga <155563722+ashwinigaikwad-ga@users.noreply.github.com>
2024-01-09 12:23:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,6 @@ except URLError as e:
      streamlit.error()
 
 
-streamlit.stop()
-
 streamlit.header("The fruit load contains!")
 def get_fruit_load_list():
   with my_cnx.cursor() as my_cur:
@@ -48,7 +46,7 @@ def get_fruit_load_list():
    my_data_rows= get_fruit_load_list()
    streamlit.dataframe(my_data_rows)
 
-
+streamlit.stop()
 
 # add_my_fruit = streamlit.text_input('What fruit would you like information about?','Jackfruit')
 # streamlit.write('The user entered ', add_my_fruit)
